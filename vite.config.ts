118f3b0208f469e1ddb37f1fbd0eaa35ae5dde8e
Girisha P <pgirisha77@gmail.com>
2026-04-11 04:17:23 +0530

    move files on <

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,6 @@ export default defineConfig({
     },
   },
   root: path.resolve(import.meta.dirname, "client"),
-  base: '/my-portfolio/',
   build: {
     outDir: path.resolve(import.meta.dirname, "dist"),
     emptyOutDir: true,
